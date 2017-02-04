4ed20608a861a5243175b9b5a299ebfe1656052d
sanjeev <sanjeevkmsinha@gmail.com>
2017-02-04 13:50:14 +0530

    change the config

--- gps_init.c
+++ gps_init.c
@@ -7,7 +7,7 @@
 void Init_GPS_GSM_Module()
 {
 sunxi_gpio_init();
-sunxi_gpio_set_cfgpin(SUNXI_GPA(17), SUNXI_GPIO_OUTPUT);
+sunxi_gpio_set_cfgpin(SUNXI_GPA(29), SUNXI_GPIO_OUTPUT);
 sunxi_gpio_output(SUNXI_GPA(29), 1);
 sleep(2);
 sunxi_gpio_output(SUNXI_GPA(29), 0);

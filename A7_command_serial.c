d8a436fca33353519e7823a0ab07ea599ceef877
sanjeev <sanjeevkmsinha@gmail.com>
2017-02-06 13:57:17 +0530

    aaa

--- A7_command_serial.c
+++ A7_command_serial.c
@@ -12,7 +12,8 @@
 #define TRUE 1
 
 int fd=0, n;
-struct termios oldtp, newtp;
+
+struct termios SerialPortSettings;
 
 void A7_command_readport(void)
 {
@@ -37,9 +38,9 @@ void A7_command_writeport(unsigned char * buff)
 {
   int buff_len;
   
-  buff_len = strlen(buff);
+  buff_len = sizeof (buff);
 
-  n = write(fd, buff, buff_len );
+  n = write(fd, buff, buff_len + 1);
 
                 if (n < 0)
                 {
@@ -59,7 +60,7 @@ void A7_command_closeport(void)
 }
 void A7_command_openport(void)
 {
-	 fd = open(MODEMDEVICE, O_RDWR | O_NOCTTY |O_NDELAY );
+	 fd = open(MODEMDEVICE, O_RDWR | O_NOCTTY );
 	 
          if (fd < 0)
          {
@@ -68,40 +69,11 @@ void A7_command_openport(void)
          }
 		 else
 			 printf("\A7 Command port open :%d\n",fd);
-                                                                                
-         fcntl(fd,F_SETFL,0);
-         tcgetattr(fd,&oldtp); /* save current serial port settings */
-    //   tcgetattr(fd,&newtp); /* save current serial port settings */
-         bzero(&newtp, sizeof(newtp));
-    //   bzero(&oldtp, sizeof(oldtp));
-                                                                                
-         newtp.c_cflag = BAUDRATE | CRTSCTS | CS8 | CLOCAL | CREAD;
-                                                                                
-         newtp.c_iflag = IGNPAR | ICRNL;
-                                                                                
-         newtp.c_oflag = 0;                                                                        
-         newtp.c_lflag = ICANON;                                                                    
-         newtp.c_cc[VINTR]    = 0;     /* Ctrl-c */
-         newtp.c_cc[VQUIT]    = 0;     /* Ctrl-\ */
-         newtp.c_cc[VERASE]   = 0;     /* del */
-         newtp.c_cc[VKILL]    = 0;     /* @ */
-        // newtp.c_cc[VEOF]     = 4;     /* Ctrl-d */
-         newtp.c_cc[VTIME]    = 0;     /* inter-character timer unused */
-         newtp.c_cc[VMIN]     = 0;     /* blocking read until 1 character arrives */
-         newtp.c_cc[VSWTC]    = 0;     /* '\0' */
-         newtp.c_cc[VSTART]   = 0;     /* Ctrl-q */
-         newtp.c_cc[VSTOP]    = 0;     /* Ctrl-s */
-         newtp.c_cc[VSUSP]    = 0;     /* Ctrl-z */
-         newtp.c_cc[VEOL]     = 0;     /* '\0' */
-         newtp.c_cc[VREPRINT] = 0;     /* Ctrl-r */
-         newtp.c_cc[VDISCARD] = 0;     /* Ctrl-u */
-         newtp.c_cc[VWERASE]  = 0;     /* Ctrl-w */
-         newtp.c_cc[VLNEXT]   = 0;     /* Ctrl-v */
-         newtp.c_cc[VEOL2]    = 0;     /* '\0' */
-   	                                                                                                                                             
-//	  tcflush(fd, TCIFLUSH);
-	 tcsetattr(fd,TCSANOW,&newtp);
 
+		tcgetattr(fd, &SerialPortSettings);		 
+
+		cfsetispeed(&SerialPortSettings,B9600);
+        cfsetospeed(&SerialPortSettings,B9600);
      
 }
 

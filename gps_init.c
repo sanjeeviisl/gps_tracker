c13db8a13482eaa6e82170c95735434367d633ed
sanjeev <sanjeevkmsinha@gmail.com>
2017-02-06 12:49:19 +0530

    GPIO PIN is set to 7 from 29

--- gps_init.c
+++ gps_init.c
@@ -7,12 +7,10 @@
 void Init_GPS_GSM_Module()
 {
 sunxi_gpio_init();
-printf("gpio init\n");
-sunxi_gpio_set_cfgpin(SUNXI_GPA(29), SUNXI_GPIO_OUTPUT);
-sunxi_gpio_output(SUNXI_GPA(29), 1);
-printf("gpio ON\n");
+sunxi_gpio_set_cfgpin(SUNXI_GPA(7), SUNXI_GPIO_OUTPUT);
+sunxi_gpio_output(SUNXI_GPA(7), 1);
 sleep(4);
-sunxi_gpio_output(SUNXI_GPA(29), 0);
-printf("gpio OFF\n");
+sunxi_gpio_output(SUNXI_GPA(7), 0);
+printf("GPS ON\n");
 sleep(1);
 }

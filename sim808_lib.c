934d503f44040d9f110af58c283471f736bf5a0e
sanjeev <sanjeevkmsinha@gmail.com>
2017-02-14 16:45:12 +0530

    NIMEA DATA IS Updated

--- sim808_lib.c
+++ sim808_lib.c
@@ -153,9 +153,10 @@ return(0);
 int GPSSim808Power(int ON) {
 
 
-char gps_power_string1[]= "AT+POWER=1\r\n";
-char gps_power_string2[]= "AT+POWER=0\r\n";
-char gps_power_string3[]= "AT\r\n";
+char gps_power_string1[]= "AT+CGPSPWR=1\r\n";
+char gps_power_string2[]= "AT+CGPSPWR=0\r\n";
+char gps_power_string3[]= "AT+CGPSSTATUS?\r\n";
+char gps_power_string4[]= "AT+CGPSINF=0\r\n";
 
 
 if(ON)
@@ -184,6 +185,7 @@ printf("%s",gps_power_string2);
 }
 
 
+
 printf("%s",gps_power_string3);
 
     RS232_cputs(cport_nr, gps_power_string3);
@@ -193,6 +195,14 @@ printf("%s",gps_power_string3);
     if(MapForward(buf,buf_SIZE,(unsigned char*)OKToken,2) == NULL)
         goto exit;
 
+printf("%s",gps_power_string4);
+
+    RS232_cputs(cport_nr, gps_power_string4);
+    Resetbufer(buf,sizeof(buf));
+    ReadComport(cport_nr,buf,6000,500000);
+    // Check if "OK" string is present in the received data 
+    if(MapForward(buf,buf_SIZE,(unsigned char*)OKToken,2) == NULL)
+        goto exit;
 
 SUCCESS: printf("\nGPS POWER SUCCESS");
 return(1);
@@ -207,9 +217,9 @@ return(0);
 int GPSSim808NIMEAData(int ON) {
 
 
-char nimea_data_string1[]= "??1\r\n"; //NIMEA DATA ON
-char nimea_data_string2[]= "??\r\n";  //NIMEA DATA OFF
-char nimea_data_string3[]= "??\r\n";  //NIMEA DATA STATUS
+char nimea_data_string1[]= "AT+CGPSOUT=8\r\n"; //NIMEA DATA ON
+char nimea_data_string2[]= "AT+CGPSOUT=0\r\n";  //NIMEA DATA OFF
+char nimea_data_string3[]= "AT+CGPSPWR=0\r\n";  //NIMEA DATA STATUS
 
 
 if(ON)
@@ -235,7 +245,6 @@ printf("%s",nimea_data_string2);
     // Check if "OK" string is present in the received data 
     if(MapForward(buf,buf_SIZE,(unsigned char*)OKToken,2) == NULL)
         goto exit;
-}
 
 
 printf("%s",nimea_data_string3);
@@ -247,6 +256,7 @@ printf("%s",nimea_data_string3);
     if(MapForward(buf,buf_SIZE,(unsigned char*)OKToken,2) == NULL)
         goto exit;
 
+}
 
 SUCCESS: printf("\nNIMEA DATA SUCCESS");
 return(1);

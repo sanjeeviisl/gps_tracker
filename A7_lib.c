18c3454832ef88ac4fc29bf107f473625cf97320
Your Name <you@example.com>
2017-03-02 22:32:20 +0530

    aaa

--- A7_lib.c
+++ A7_lib.c
@@ -353,6 +353,7 @@ int A7DataConnect() {
 
 	restart:
 
+		if(!A7_dataConnected {
 		n++;
 		
 		RS232_cputs(A7_commond_cport_nr, data_connect_string1);
@@ -392,7 +393,7 @@ int A7DataConnect() {
 		ReadComport(A7_commond_cport_nr,A7_buf,6000,500000);
 		if(MapForward(A7_buf,A7_buf_SIZE,(unsigned char*)A7_OKToken,2) == NULL)
 			goto exit;
-
+                  }
 	
 	
 		RS232_cputs(A7_commond_cport_nr, data_connect_string6);
@@ -491,8 +492,8 @@ restart:
 
 //snprintf(send_string,sizeof(send_string), "%s%s%s%s%s%s%s%s%s%s", tcp_header_str,"device_id=",A7_device_id_str,"&","latitude=",A7_latitude_str,"&" , "longitude=",A7_longitude_str,tcp_body_str);
 
-snprintf(send_string,sizeof(send_string),"%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s", tcp_header_str,"device_id=",A7_device_id_str,"&","latitude=",A7_latitude_str,"&","longitude=",A7_longitude_str,"&","utcdate_stamp=",A7_updated_date_str,"&","utctime_stamp=",A7_updated_time_str,tcp_body_str);
-			
+		snprintf(send_string,sizeof(send_string),"%s%s%s%s%s%s%s%s%s%s%s%s", tcp_header_str,"device_id=",A7_device_id_str,"&latitude=",A7_latitude_str,"&longitude=",A7_longitude_str,"&utcdate_stamp=",A7_updated_time_str,"&utctime_stamp=",A7_updated_time_str,tcp_body_str);
+	
 			RS232_cputs(A7_commond_cport_nr, send_string);
 //			RS232_cputs(A7_commond_cport_nr, send_string1);
 			RS232_cputs(A7_commond_cport_nr, tcp_footer_str);
@@ -608,8 +609,9 @@ int sendA7StatusToTCPServer(int testData)
 	
 	
 	//snprintf(send_string,sizeof(send_string), "%s%s%s%s%s%s%s%s%s%s", tcp_header_str,"device_id=",A7_device_id_str,"&","latitude=",A7_latitude_str,"&" , "longitude=",A7_longitude_str,tcp_body_str);
-snprintf(send_string,sizeof(send_string),"%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s", tcp_header_str,"device_id=",A7_device_id_str,"&","latitude=",A7_latitude_str,"&","longitude=",A7_longitude_str,"&","utcdate_stamp=",A7_updated_date_str,"&","utctime_stamp=",A7_updated_time_str,tcp_body_str);	
-				
+			
+snprintf(send_string,sizeof(send_string),"%s%s%s%s%s%s%s%s%s%s%s%s", tcp_header_str,"device_id=",A7_device_id_str,"&latitude=",A7_latitude_str,"&longitude=",A7_longitude_str,"&utcdate_stamp=",A7_updated_time_str,"&utctime_stamp=",A7_updated_time_str,tcp_body_str);
+	
 				RS232_cputs(A7_commond_cport_nr, send_string);
 	//			RS232_cputs(A7_commond_cport_nr, send_string1);
 				RS232_cputs(A7_commond_cport_nr, tcp_footer_str);
